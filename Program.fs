8969378427a57800928f5909c67e8dfab8b925fe
Espen Kirkesæther Brun <espen_brun@hotmail.com>
2023-07-28 22:59:26 +0200

    Add BloggingModel

--- Program.fs
+++ Program.fs
@@ -1,2 +1,9 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open FsEfTest
+open BloggingModel
+
+// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+let context = new BloggingContext()
+
+printfn "Done"
